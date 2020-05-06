a1ec91e27c7eb3ab4e4b5c3c602fc8b542a25b81
arun chauhan <arunfaceyou@gmail.com>
2020-05-06 20:33:28 +0530

    Updates

---
+++ precache-manifest.966314d0484ec732f00b4820e99a0dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6b59ae8d6ac8a1b322bd018ab4eb7e",
+    "url": "/aka/index.html"
+  },
+  {
+    "revision": "434053380e8b5990f054",
+    "url": "/aka/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ab00f67d9507e833f646",
+    "url": "/aka/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "434053380e8b5990f054",
+    "url": "/aka/static/js/2.02aba45c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/aka/static/js/2.02aba45c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab00f67d9507e833f646",
+    "url": "/aka/static/js/main.7bc494b0.chunk.js"
+  },
+  {
+    "revision": "8dc8ae4105fcc2916156",
+    "url": "/aka/static/js/runtime-main.2769e9d3.js"
+  }
+]);
